c23f4c6a745a7abc3b2d3f6dc907ce113a7c35bd
MOYETTCLARK <m.clark150@verizon.net>
2017-08-21 14:41:50 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure( HttpSecurity http ) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/CSS/**", "/js/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure( AuthenticationManagerBuilder auth ) throws Exception {
+
+            auth.inMemoryAuthentication().
+               withUser("user").password("password").roles("USER");
+
+            // to add additional accounts, remove the semicolon at
+            // end of previous command & add additional users like below:
+            //      .and()
+            //     .withUser("dave").password("begreat").roles("USER");
+
+    }
+
+}
